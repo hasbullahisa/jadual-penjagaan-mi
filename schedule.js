67a87ba84f5d76858f51ca4bc45e485697d55a89
hasbullahisa <putraaiman@gmail.com>
2025-12-31 22:36:37 +0800

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -5,12 +5,13 @@ window.SCHEDULE = {
   title: "Jadual Penjagaan Mi (Wad Hospital Jerantut)",
   timezone: "Asia/Kuala_Lumpur",
   windowStart: "2025-12-28T20:00:00",
-  windowEnd: "2025-12-31T20:00:00",
+  windowEnd: "2026-01-03T20:00:00",
   slots: [
     // Night shifts (Bibik Yuni)
     { start: "2025-12-28T20:00:00", end: "2025-12-29T08:00:00", person: "Bibik Yuni", label: "Malam" },
     { start: "2025-12-29T20:00:00", end: "2025-12-30T08:00:00", person: "Bibik Yuni", label: "Malam" },
     { start: "2025-12-30T20:00:00", end: "2025-12-31T08:00:00", person: "Bibik Yuni", label: "Malam" },
+    { start: "2025-12-31T20:00:00", end: "2026-01-01T08:00:00", person: "Bibik Yuni", label: "Malam" },
 
     // Morning shifts (Kak Jah)
     { start: "2025-12-29T08:00:00", end: "2025-12-29T14:00:00", person: "Kak Jah", label: "Pagi" },
